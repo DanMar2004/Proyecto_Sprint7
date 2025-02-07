8169ba18401e64d20db7cb99a774821168f96cc2
DanMar2004 <daniel200498@outlook.es>
2025-02-06 22:08:21 -0600

    Version finla 1.0

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Proyecto7
 Proyecto de Sprint 7 Tripleten
+Para el proyecto pondremos en practica diferentes competencias aprendidas en el sprint:
+- Creacion de repositirio en GitHub
+- Desplegar el codigo de python en Render
+- Creacion de entorno virtual con la instalacion de paquetes 
+- Usar IDE como Visual Estudio Code
+
+Concluimos que con las herramientas aprendidas en este sprint tenemos un set de herramientas que nos facilitan el acceso a los archivos a su codificacion, facilita el poder trabajar en archivos y posteiormente actualizarlos para su continuo uso.
--- app.py
+++ app.py
@@ -6,12 +6,19 @@ car_data = pd.read_csv('vehicles_us.csv')  # leer los datos
 
 st.header('Graficos de Vehiculos')
 
-build_histo = st.checkbox('Construir histograma')
+build_histo = st.button('Construir histograma')
 if build_histo:
     st.write(
-        'Construir histograma para el conjunto de datos de anuncios de ventas de coches')
+        'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
 
     fig = px.histogram(car_data, x="odometer")
 
     st.plotly_chart(fig, use_container_width=True)
 
+build_scatter = st.checkbox('Construir grafico de dispersion')
+if build_scatter:
+    st.write('Creación de un grafico de dispersion para el conjunto de datos de anuncios de venta de coches')
+    
+    fig = px.scatter(car_data, x="odometer", y="price")
+
+    st.plotly_chart(fig, use_container_width=True)
